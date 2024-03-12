5d9b14236aba2add6dd0bf16c6148483d635dade
Vivek Vaishnani <91829247+VivekVaishnanai07@users.noreply.github.com>
2024-03-12 11:57:49 +0530

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
-host: "localhost",
-user: "root",
-password: "root",
-database:"dpl_11" 
+host: "bxfyzxawyspkusfujgy1-mysql.services.clever-cloud.com",
+user: "u6i9cjns8adyuu7r",
+password: "f1nlUJTkJ5bIbpzZXLaE",
+database:"bxfyzxawyspkusfujgy1" 
 })
 
 module.exports = db;
