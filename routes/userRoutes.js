219bbe73625ebeeb6876776ffd3899ea861abf3d
Vivek Vaishnani <vivek.vaishnani@deltafixes.com>
2024-04-05 12:08:39 +0530

    1. Added bodyParser in package 2. Added userRoute in verifyRoleOrToken & edit user profile created

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const db = require('../config/db.config');
+const verifyRoleOrToken = require('../middlewares/verifyRoleOrToken');
 
 const router = express.Router();
 
 // Get all users
-router.get("/", (req, res) => {
-  db.query("SELECT * FROM users", (err, result) => {
+router.get("/", verifyRoleOrToken(['admin']), (req, res) => {
+  db.query("SELECT id, email, first_name, last_name, role, userImg FROM users", (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).send("Internal Server Error");
@@ -16,20 +17,20 @@ router.get("/", (req, res) => {
 });
 
 // Get one user by id
-router.get("/:id", (req, res) => {
+router.get("/:id", verifyRoleOrToken(['admin']), (req, res) => {
   const id = req.params.id;
-  db.query("SELECT * FROM users WHERE id = ?", id, (err, result) => {
+  db.query("SELECT id, email, first_name, last_name, role, userImg FROM users WHERE id = ?", id, (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).send("Internal Server Error");
       return;
     }
-    res.send(result);
+    res.send(result[0]);
   });
 });
 
 // creating new user
-router.post('/add-user', (req, res) => {
+router.post('/add-user', verifyRoleOrToken(['admin']), (req, res) => {
   const updateData = req.body;
   db.query(`INSERT INTO users (first_name,last_name,email,password,role) VALUES ('${updateData.first_name}','${updateData.last_name}','${updateData.email}','Test@123','${updateData.role}')`, (err, result) => {
     if (err) {
@@ -41,10 +42,10 @@ router.post('/add-user', (req, res) => {
 })
 
 // edit user
-router.put('/:id', (req, res) => {
+router.put('/:id', verifyRoleOrToken(['admin']), (req, res) => {
   const id = req.params.id;
   const updateData = req.body;
-  const sql = `UPDATE users SET first_name = '${updateData.first_name}',last_name = '${updateData.last_name}',email = '${updateData.email}',role = '${updateData.role}' WHERE id = ${id}`;
+  const sql = `UPDATE users SET first_name = '${updateData.first_name}',last_name = '${updateData.last_name}',email = '${updateData.email}',role = '${updateData.role}',userImg = '${updateData.userImg}' WHERE id = ${id}`;
   db.query(sql, (err, result) => {
     if (err) {
       console.error(err);
@@ -55,8 +56,23 @@ router.put('/:id', (req, res) => {
   });
 });
 
+// edit user profile
+router.put('/profile/:id', verifyRoleOrToken(['admin', 'user']), (req, res) => {
+  const id = req.params.id;
+  const updateData = req.body;
+  const sql = `UPDATE users SET first_name = '${updateData.first_name}',last_name = '${updateData.last_name}',userImg = '${updateData.userImg}' WHERE id = ${id}`;
+  db.query(sql, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(200).json({ message: 'User profile updated successfully' });
+    }
+  });
+});
+
 // delete a user
-router.delete('/:id', (req, res) => {
+router.delete('/:id', verifyRoleOrToken(['admin']), (req, res) => {
   const id = req.params.id;
   db.query("DELETE FROM users WHERE id= ?", id, (err, result) => {
     if (err) {
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const db = require('./config/db.config');
 const jwt = require('jsonwebtoken');
 const verifyRoleOrToken = require('./middlewares/verifyRoleOrToken');
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/userRoutes');
 const teamRoutes = require('./routes/teamRoutes');
@@ -17,10 +18,11 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3300;
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
 
 // Apply token verification middleware to protected routes
-app.use("/api/user", verifyRoleOrToken(['admin']), userRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/team", teamRoutes);
 app.use("/api/match", matchRoutes);
 app.use("/api/prediction", verifyRoleOrToken(['admin', 'user']), predictionRoutes);
